8929e2864f4c59fae9169b55240b12c4488cdf2b
scolia <scolia@qq.com>
2017-08-12 22:23:03 +0800

    新增c#版简单选择排序算法

---
+++ C sharp/Algorithms/SimpleSelectionSort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    namespace Sort
+    {
+        namespace SimpleSelectionSort
+        {
+            public class SimpleSelectionSortV1<T>
+            where T : IComparable
+            {
+                /// <summary>
+                /// 升序排序
+                /// </summary>
+                /// <param name="array"></param>
+                /// <param name="length"></param>
+                public static void Sort(IList<T> array, int length)
+                {
+                    int index;
+                    for (int i = 0; i < length; i++)
+                    {
+                        index = i;
+                        for (int j = i + 1; j < length; j++)
+                        {
+                            if (array[index].CompareTo(array[j]) > 0)
+                            {
+                                index = j;
+                            }
+                        }
+                        if (i != index)
+                        {
+                            var temp = array[i];
+                            array[i] = array[index];
+                            array[index] = temp;
+                        }
+                    }
+                }
+
+                /// <summary>
+                /// 降序排序
+                /// </summary>
+                /// <param name="array"></param>
+                /// <param name="length"></param>
+                public static void SortDesc(IList<T> array, int length)
+                {
+                    int index;
+                    for (int i = 0; i < length; i++)
+                    {
+                        index = i;
+                        for (int j = i + 1; j < length; j++)
+                        {
+                            if (array[index].CompareTo(array[j]) < 0)
+                            {
+                                index = j;
+                            }
+                        }
+                        if (i != index)
+                        {
+                            var temp = array[i];
+                            array[i] = array[index];
+                            array[index] = temp;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    
+}
